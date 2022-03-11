4115a52e4ef43751165fe7abf76b3f48a08cd395
bessem azizi <azizi.bessem@esprit.tn>
2022-03-10 23:58:53 -1100

    appShe

---
+++ src/main/java/tn/esprit/project/Entities/Candidacy.java
@@ -0,0 +1,34 @@
+package tn.esprit.project.Entities;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Candidacy implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idCandacy ;
+    boolean etat ;
+    String cv ;
+
+
+    /************Relation**************/
+    @ManyToOne
+    User candidacyUser;
+    @ManyToOne
+    Offre offer;
+
+
+
+}
